0e1d82246ea4a7879cfc9bf809047bc2d0fe5529
Alex Home Laptop <alex.becker@my.lpts.edu>
2020-03-10 18:04:17 -0400

    added in menu draft with placeholder icons, need to add the off class and create a function to toggle it

--- js/main.js
+++ js/main.js
@@ -1,25 +1,7 @@
+//All DOM manipulation
 window.addEventListener('load', function () {
 
-    /*$('#mainView').after(`
-    <div id="codeView" class="main off">
-        <div class="menu">
-        <svg viewBox="0 0 100 80" width="30" height="30">
-            <rect width="100" height="15" rx="8"></rect>
-            <rect y="30" width="100" height="15" rx="8"></rect>
-            <rect y="60" width="100" height="15" rx="8"></rect>
-        </svg>
-        </div>
-        <div class="login">Login</div>
-        <div class="siteTitle">The Tidy Squirrel 🐿️</div>
-        <div class="joinButton button blueBtn">Join</div>
-        <div id="codeForm" class="form">
-            <div id="cfTitle" class="inputTitle">Enter Code</div>
-            <div id="codeInput" class="textInput"></div>
-            <div id="codeSubmit" class="submitButton button greyBtn">Submit</div>
-        </div>
-    </div>
-    `);*/
-
+    //Join button click handler
     document.getElementById('joinButton').addEventListener('click', function(el) {
         console.log('join click');
         $("#codeInputGroup").removeClass("off");
@@ -36,24 +18,10 @@ window.addEventListener('load', function () {
             }, 600);
         }, 10);
     });
+
+    //Text input clear button handler
     $('.textClear').click(this, function(el) {
         $(this).parent().children('input').first().val('');
         console.log($(this).parent().children('input').first().val());
     });
 });
-
-
-
-/*window.addEventListener('click', function(el) {
-    console.log("login");
-    $("#mainView").css("opacity", "0%");
-    $("#codeView").css("opacity", "0%");
-    this.setTimeout(function () {
-        $("#mainView").addClass("off");
-        $("#codeView").removeClass("off");
-        this.setTimeout(function() {
-            $("#codeView").css("opacity", "100%");
-        }, 10);
-    }, 600);
-    
-});*/

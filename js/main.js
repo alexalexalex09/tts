b10d5147cfcdae8ff72eb81f2734aa2bb5e77c04
Alex Home Laptop <alex.becker@my.lpts.edu>
2020-03-09 22:13:19 -0400

    new transition to codeInput

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function () {
 
-    $('#mainView').after(`
+    /*$('#mainView').after(`
     <div id="codeView" class="main off">
         <div class="menu">
         <svg viewBox="0 0 100 80" width="30" height="30">
@@ -18,10 +18,26 @@ window.addEventListener('load', function () {
             <div id="codeSubmit" class="submitButton button greyBtn">Submit</div>
         </div>
     </div>
-    `);
+    `);*/
+
+    document.getElementById('joinButton').addEventListener('click', function(el) {
+        console.log('join click');
+        $("#codeInputGroup").removeClass("off");
+        window.setTimeout(function() {
+            console.log('wait 1')
+            $('#createButton').css("opacity", "0%");
+            $("#codeInputGroup").css("opacity", "100%");
+            window.setTimeout(function () {
+                console.log('wait 2')
+                $("#createButton").addClass("off");
+            }, 1100);
+        }, 10);
+    });
 });
 
-window.addEventListener('click', function(el) {
+
+
+/*window.addEventListener('click', function(el) {
     console.log("login");
     $("#mainView").css("opacity", "0%");
     $("#codeView").css("opacity", "0%");
@@ -33,4 +49,4 @@ window.addEventListener('click', function(el) {
         }, 10);
     }, 600);
     
-});
+});*/

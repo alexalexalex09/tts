7a7a7a6f77ca3711158e78eaf3e597619a7f105a
Alex Desktop <alexscottbecker@gmail.com>
2020-03-11 08:45:04 -0400

    finished Okta integration with tutorial at https://www.digitalocean.com/community/tutorials/build-and-understand-a-simple-nodejs-website-with-user-authentication , TODO: convert current HTML pages to Pug (formerly Jade) views

--- login-portal/app.js
+++ login-portal/app.js
@@ -4,11 +4,37 @@ var path = require('path');
 //var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require("express-session");
+var okta = require("@okta/okta-sdk-nodejs");
+var ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+const dashboardRouter = require("./routes/dashboard");
+const publicRouter = require("./routes/public");
+const usersRouter = require("./routes/users");
 
 var app = express();
+
+var oktaClient = new okta.Client({
+  orgUrl: 'https://dev-222844.okta.com',
+  token: '00T-POzgNn1N2HdBngzKuLiA0a8VynCvD8gQGsFCkr'
+});
+const oidc = new ExpressOIDC({
+  issuer: "https://dev-222844.okta.com/oauth2/default",
+  client_id: '0oa3ate5jDukoR2LH4x6',
+  client_secret: 'SaLJUuCshNXVz2CE3k7hcd5O-WpilWVSsoeGnD8-',
+  redirect_uri: 'http://localhost:3000/users/callback',
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "/users/login"
+    },
+    callback: {
+      path: "/users/callback",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,9 +50,35 @@ app.use(session({
   resave: true,
   saveUninitialized: false
 }));
+app.use(oidc.router);
+app.use((req, res, next) => {
+  if (!req.userinfo) {
+    return next();
+  }
+
+  oktaClient.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      res.locals.user = user;
+      next();
+    }).catch(err => {
+      next(err);
+    });
+});
+
+function loginRequired(req, res, next) {
+  if (!req.user) {
+    return res.status(401).render("unauthenticated");
+  }
+
+  next();
+}
 
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+app.use('/', publicRouter);
+app.use('/dashboard', loginRequired, dashboardRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler

7e37a0bfdb1cf4f8bbcb618c7ad8bcfcb99fcd49
Alex Home Laptop <alex.becker@my.lpts.edu>
2020-03-17 19:07:34 -0400

    beginning to add sql stuff to public.js route

--- login-portal/routes/public.js
+++ login-portal/routes/public.js
@@ -1,7 +1,28 @@
 const express = require("express");
-
+//const mysql = require('mysql');
 
 const router = express.Router();
+
+function getMySQLConnection() {
+	return mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'alexmufm_tts',
+	  password : 'UusqLhw&uk,1',
+	  database : 'alexmufm_tts_test'
+	});
+}
+
+var connection = getMySQLConnection();
+connection.connect(function(err) {
+  if (err) throw err;
+  // this isn't ready yet:
+  // connection.query('SELECT * from "users" where "id" = '+user.id)
+  // if there's nothing in the result, add a new row, populating it with data from Okta's user object
+  //
+});
+
+
+
 
 // Home page
 router.get("/", (req, res) => {

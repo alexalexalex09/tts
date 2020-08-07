960de286a8b404430082abdb5add2c401c63cef1
Alex <abecker@covenantfrazer.org>
2020-08-07 08:47:00 -0400

    Adding in resources.js

---
+++ models/resources.js
@@ -0,0 +1,29 @@
+//Set up mongoose connection
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+//Create schema
+var ResourceSchema = new Schema(
+  {
+    name: String,
+    data: Object,
+  },
+  {
+    collection: "resources",
+  }
+);
+
+module.exports = mongoose.model("resource", ResourceSchema);
+/*
+
+//Export function to create "SomeModel" model class
+module.exports = mongoose.model('SomeModel', SomeModelSchema );
+
+You can then require and use the model immediately in other files. Below we show how you might use it to get all instances of the model.
+
+//Create a SomeModel model just by requiring the module
+var SomeModel = require('../models/somemodel')
+
+// Use the SomeModel object (model) to find all SomeModel records
+SomeModel.find(callback_function);
+*/
